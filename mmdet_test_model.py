f4e3cea1af2ece13cfadfc2c153e198125509dbc
Vincent Lin <60536661+vincnttt@users.noreply.github.com>
2022-12-17 15:00:27 +0800

    Create mmdet_test_model.py

---
+++ mmdet_test_model.py
@@ -0,0 +1,17 @@
+from mmdet.apis import init_detector, inference_detector
+import mmcv
+
+# Specify the path to model config and checkpoint file
+config_file = 'configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'  # path of config file
+checkpoint_file = 'checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'   # path of trained output
+
+# build the model from a config file and a checkpoint file
+model = init_detector(config_file, checkpoint_file, device='cuda:0')
+
+# test a single image and show the results
+img = 'test.jpg'  # or img = mmcv.imread(img), which will only load it once
+result = inference_detector(model, img)
+# visualize the results in a new window
+model.show_result(img, result)
+# or save the visualization results to image files
+model.show_result(img, result, out_file='result.jpg')
